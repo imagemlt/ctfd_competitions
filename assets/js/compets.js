af24656826812ddb3e38411308e124d72722816d
imagemlt <malingtao1019@163.com>
2018-03-08 19:29:14 +0800

    adjusted assets

--- assets/js/compets.js
+++ assets/js/compets.js
@@ -7,7 +7,7 @@ function update() {
         'success': function (data) {
             console.log(data);
             competitions = data;
-            $.get(script_root + '/plugins/competitions/assets/compet.hbs', function (temp) {
+            $.get(script_root + '/plugins/competitions/assets/js/compet.njk', function (temp) {
                 var template = nunjucks.compile(temp);
                 var wrapper = { competitions: competitions.competitions ,script_root:script_root};
                 $('#content').html(template.render(wrapper));
